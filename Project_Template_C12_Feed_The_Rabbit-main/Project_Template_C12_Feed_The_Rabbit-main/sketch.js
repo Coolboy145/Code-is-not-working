bcd5646e12f33f3a288875f15f517a852f695cc6
Coolboy145 <88191598+Coolboy145@users.noreply.github.com>
2021-08-31 13:40:00 +0100

    Add files via upload

---
+++ Project_Template_C12_Feed_The_Rabbit-main/Project_Template_C12_Feed_The_Rabbit-main/sketch.js
@@ -0,0 +1,46 @@
+var garden,rabbit,apple,leaf;
+var gardenImg,rabbitImg,appleImg,leafImg;
+
+var select_sprites = Math.round(random(1,2));
+
+
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+  leafImg = loadImage("leaf.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+  // Moving background
+  garden=createSprite(200,200);
+  garden.addImage(gardenImg);
+
+  //creating boy running
+  rabbit = createSprite(180,340,30,30);
+  rabbit.scale =0.09;
+  rabbit.addImage(rabbitImg);
+  
+}
+
+
+function ctreateApples(){
+ if(frameCount% 100==0){
+ var apple = createSprite(100,200,20,20)
+ apple.velocityY=3;
+}
+
+ 
+ 
+function draw() {
+  background(0);
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  drawSprites();
+}
